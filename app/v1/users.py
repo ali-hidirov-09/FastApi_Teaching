c8396be959067a9d16f0bca50a28ee23c5fba0bf
Khidirov Ali <alihidirov285@gmail.com>
2026-06-19 18:00:19 +0500

    students were taught by Ali

--- app/v1/users.py
+++ app/v1/users.py
@@ -1,10 +1,104 @@
 from typing import Optional
-
+from pydantic import BaseModel, SecretStr, ConfigDict, Field, EmailStr
+from typing import Annotated
 from fastapi import APIRouter, Path, Query
+from pydantic.alias_generators import to_camel
 
 router = APIRouter()
 
 
+class BaseSchema(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+        extra='forbid'
+    )
+
+MinStr = Annotated[str, Field(min_length=3)]
+MinInt = Annotated[int, Field(gt=0)]
+#------------------------------------------Dars_5------------------------------------------------------------
+
+
+class UserSchema(BaseSchema):
+    username: str = Field(min_length=3)
+    is_active: bool = True
+    email: EmailStr
+    password: SecretStr
+
+
+user = UserSchema(username="Ali", email="Ali@gmail.com", password="448484646887")
+user_data = user.model_dump()
+user_data1 = user.model_dump_json(exclude={"password"})
+print(user.password.get_secret_value())
+print(user_data1)
+
+
+
+class JobSchema(BaseSchema):
+    id: MinInt
+    title: str
+    salary_min: float = Field(gt=0)
+    salary_max: float = Field(lt=5000)
+
+
+
+@router.post('/job')
+async def job_create_1(job:JobSchema):
+    jod_data = job.model_dump()
+    return {
+        "message": "Muvaffaqiyatli yaratildi",
+        "data": jod_data
+    }
+
+
+@router.post('/user')
+async def user_create_1(user:UserSchema):
+    user_data = user.model_dump()
+    return {
+        "message": "Muvaffaqiyatli yaratildi",
+        "data": user_data
+    }
+
+
+
+devs = [
+    {
+        "id": 1,
+        "category":"IT",
+        "title": "Python",
+        "full_name":"Ali Khidirov",
+        "experience": 1,
+        "salary": 600.0,
+        "description": "Men junior python developerman va kompaniya rivoji uchun hamma narsa qilishga tayyorman."
+    }
+]
+
+class Job(BaseSchema):
+    category: MinStr
+    title: MinStr
+    is_active: bool = True
+    full_name: MinStr
+    experience: int | None = None
+    salary: float = Field(..., ge=0.0)
+    description: MinStr
+    created_at: MinStr
+    updated_at: MinStr
+
+
+
+@router.post('/dev_create')
+async def dev_create(job: Job):
+    job_data = job.model_dump()
+    job_data['id'] = len(devs)+1
+    devs.append(job_data)
+    print(devs)
+    return {"message": "Muvaffaqiyatli yaratildi",
+            "data": job_data}
+
+
+
+
+#------------------------------------------Dars_2,3------------------------------------------------------------
 @router.get("/get_name")
 def get_name():
     return {
